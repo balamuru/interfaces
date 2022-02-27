b5bff4b0d847929b67bbc04da70beb65378602aa
balamuru <balamuru@gmail.com>
2022-02-27 00:56:19 -0600

    cleanup

--- driver/app.go
+++ driver/app.go
@@ -1,28 +1,24 @@
 package main
+
 import (
-	"fmt"
 	"github.com/balamuru/interfaces/animal"
 )
-
-
-
 
 type Animal2 interface {
 	Move() string
 	Eat() string
-
 }
 
 type Horse struct {
 	walkBehavior string
-	eatBehavior string
+	eatBehavior  string
 }
 
-func (h Horse) Move() string{
+func (h Horse) Move() string {
 	return h.walkBehavior
 }
 
-func (h Horse) Eat() string{
+func (h Horse) Eat() string {
 	return h.eatBehavior
 }
 
@@ -34,13 +30,12 @@ func main() {
 	println(horsie.eatBehavior)
 
 	//impl interface from aother package
-	var animal animal.Animal = horsie 
+	var animal animal.Animal = horsie
 	println(animal.Move())
 	println(animal.Eat())
 
-
 	//impl interface defined in current pkg
-	var animal2 Animal2 = horsie 
+	var animal2 Animal2 = horsie
 	println(animal2.Move())
 	println(animal2.Eat())
 }
